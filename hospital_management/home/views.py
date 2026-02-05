9b73476a05b5e6c71eba8ffbc735ae24c7dfa8c1
Arjun <arjuni5acer12@gmail.com>
2026-02-06 01:30:10 +0530

    Initial commit

---
+++ hospital_management/home/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import BookingForm
+from .models import Department, Doctors
+from django.core.mail import send_mail
+
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def booking(request):
+    if request.method == 'POST':
+        form = BookingForm(request.POST)
+        if form.is_valid():
+            booking = form.save()
+
+            subject = f'New Appointment Booking - {booking.p_name}'
+            message = f'''
+       A new booking has been made:
+
+       Name: {booking.p_name}
+       Phone: {booking.p_phone}
+       Email: {booking.p_email}
+       Doctor: {booking.doc_name}
+       Date: {booking.booking_date}
+       Booked On: {booking.booked_on}
+            '''
+
+            send_mail(
+                subject,
+                message,
+                'your-email@gmail.com',
+                ['recipient-email@example.com'],
+                fail_silently=False,
+        )
+
+            return render(request, 'confirmation.html')
+    else:
+        form = BookingForm()
+
+    return render(request, 'booking.html', {'form': form})
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def department(request):
+    return render(request, 'department.html', {
+        'dept': Department.objects.all()
+    })
+
+def doctors(request):
+    return render(request, 'doctors.html', {
+        'doctors': Doctors.objects.all()
+    })
