1128ac2a1aa26218bc46399a86e5b0f97b4e3227
Arjun <arjuni5acer12@gmail.com>
2026-02-07 23:20:56 +0530

    pq

--- hospital_management/home/apps.py
+++ hospital_management/home/apps.py
@@ -3,6 +3,6 @@ from django.apps import AppConfig
 
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hospital_management.home'
+    name = 'home'
 
 
