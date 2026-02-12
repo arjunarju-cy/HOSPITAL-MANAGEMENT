6e743ec8eb274357ce2825a9579768c0f0d3dd3f
Arjun <arjuni5acer12@gmail.com>
2026-02-12 20:05:23 +0530

    eheh

--- hospital_management/hospital_management/settings.py
+++ hospital_management/hospital_management/settings.py
@@ -161,10 +161,15 @@ CRISPY_TEMPLATE_PACKS = 'bootstrap5'
 
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+if os.environ.get("RENDER"):
+    # On Render (production) → do NOT use SMTP
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+else:
+    # On local machine → use Gmail
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+    DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
